9b272f73a925d80c21bb2973db403383d889c2a7
Gergana Mileva <37107768+allsi89@users.noreply.github.com>
2018-12-08 15:33:21 +0200

    Update RacerRepository.java

--- MostWanted/src/main/java/mostwanted/repository/RacerRepository.java
+++ MostWanted/src/main/java/mostwanted/repository/RacerRepository.java
@@ -10,19 +10,8 @@ import java.util.Optional;
 
 @Repository
 public interface RacerRepository extends JpaRepository<Racer, Integer> {
-    // TODO : Export Racers with their racing cars
 
     Optional<Racer> findByName(String name);
-
-    /*
-    Export all racers which have any cars:
-•	Export the racer’s name, age (but ONLY if it is NOT NULL), list of cars.
-o	In case the racer’s age property is NULL, do NOT include it.
-•	The cars should be strings in the following format: “{brand} {model} {yearOfProduction}”.
-•	Order them descending, by count of cars they have, and then by racer name alphabetically.
-
-
-     */
 
     @Query("" +
             "SELECT r FROM mostwanted.domain.entities.Racer r " +
